098951918a10fa29a5a0f382bf910f0875b01fa9
Rahul Roy <rahulroy1992.21@gmail.com>
2016-07-26 03:14:21 +0530

    Django-2: Static Files upload and templates

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS =  [
+    os.path.join(BASE_DIR, 'static')
+]
+
 TEMPLATES =  [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -137,4 +141,5 @@ TEMPLATES =  [
     },
 ]
 
-print(BASE_DIR)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
